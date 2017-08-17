5f3681b4db7859535b5e47f9d519e6889417da88
flynnkevin <7631978122qq.com>
2017-08-17 22:45:51 +0800

    增加博客评论功能
    获取iteye的博客列表，并对每篇博客进行评论

--- IteyeLogin/IteyeLogin/spiders/iteyespider.py
+++ IteyeLogin/IteyeLogin/spiders/iteyespider.py
@@ -4,9 +4,10 @@ from scrapy.http import Request,FormRequest
 from scrapy.spider import Spider
 from scrapy.selector import Selector
 from xlwt import *
-
+import re
 class IteyeSpider(Spider):
-	name="iteyespider"
+        download_delay=3
+	name="iteye_spider"
 	login_page ="http://www.iteye.com/login"
 	start_urls = ["http://kevinflynn.iteye.com/",
                   "http://kevinflynn.iteye.com/blog/2281218"]
@@ -26,10 +27,31 @@ class IteyeSpider(Spider):
                                           callback=self.logged_in)]
 
 	def logged_in(self,response):
-		return [Request(self.start_urls[0],self.get_page)]
+	#	return [Request(self.start_urls[0],self.get_page)]
+                yield Request(url="http://www.iteye.com/blogs",callback=self.parse_blog_list)
+
+        def parse_blog_list(self,response):
+                sel=Selector(response)
+                urls=sel.xpath("//div[@class='content']/h3/a/@href").extract()
+                for url in urls:
+                    yield Request(url=url,callback=self.parse_blog_page)
+
+        def parse_blog_page(self,response):
+                sel=Selector(response)
+                authenticity_token=sel.xpath("//*[@name='authenticity_token']/@value").extract_first()
+                current_url=response.url
+                print type(current_url)
+                print current_url.index("blog")
+                blog_id= current_url[current_url.index("blog")+5:]
+                commit_url="http://gaojingsong.iteye.com/blog/create_comment/"+str(blog_id)
+                commit_content="写的很好，学习了，谢谢大神的分享!!!!"
+                formdata={"comment[body]":commit_content,"commit":"提交","_":"","authenticity_token":authenticity_token}
+                yield FormRequest(url=commit_url,formdata=formdata,callback=self.parse_commit,dont_filter=True)
+
+
 
 	def get_page(self, response):
-		print response.body
+    	#	print response.body
 		sel=Selector(response)
 		titles=sel.xpath('//h3/a/text()').extract()
 		for title in titles:
@@ -45,7 +67,8 @@ class IteyeSpider(Spider):
             yield FormRequest(url=commit_url,formdata=formdata,callback=self.parse_commit,dont_filter=False)
 
         def parse_commit(self,response):
-            print response.body
+            pass
+        #    print response.body
 
 
 
